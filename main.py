70c5b4b693092e6abdaba44d3802bce8b6bece0f
free-flow-code <wildmuskaria8@gmail.com>
2024-02-06 14:05:15 +0300

    Added error handling.

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ async def write_to_file(filename, message):
 
 
 async def read_chat_messages(host, port):
-    reader, writer = await asyncio.open_connection(f'{host}', port)
     filename = 'messages.txt'
+    try:
+        reader, writer = await asyncio.open_connection(f'{host}', port)
 
-    while True:
-        data = await reader.read(1024)
-        message = f'{data.decode()!r}'
-        date_now = f"[{datetime.strftime(datetime.now(), '%d.%m.%Y %H:%M')}]"
-        print(date_now, message)
-        await write_to_file(filename, f'{date_now} {message}\n')
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            message = f'{data.decode()!r}'
+            date_now = f"[{datetime.strftime(datetime.now(), '%d.%m.%Y %H:%M')}]"
+            print(date_now, message)
+            await write_to_file(filename, f'{date_now} {message}\n')
 
-    print('Close the connection')
-    writer.close()
-    await writer.wait_closed()
+        print('Close the connection')
+        writer.close()
+        await writer.wait_closed()
+    except ConnectionError as err:
+        print(f'Connection error: {err}')
+    except asyncio.CancelledError:
+        print('Cancelled')
+    except Exception as err:
+        print(f'Unexpected error: {err}')
 
 
 def main():
