1cda4826f7fe39a484c18f1bc26a88b4818d9af5
free-flow-code <wildmuskaria8@gmail.com>
2024-02-06 15:51:33 +0300

    Add CLI options.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+import os
 import asyncio
 import aiofiles
+import argparse
+from environs import Env
 from datetime import datetime
 
+env = Env()
+env.read_env()
 
-async def write_to_file(filename, message):
-    async with aiofiles.open(filename, 'a') as file:
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Message chat.')
+    parser.add_argument('--host', default='', type=str, help='chat hostname')
+    parser.add_argument('--port', type=int, help='chat port number')
+    parser.add_argument('--history', default='messages.txt', type=str, help='chating history')
+    return parser.parse_args()
+
+
+async def write_to_file(filepath, message):
+    async with aiofiles.open(filepath, 'a') as file:
         await file.write(message)
 
 
-async def read_chat_messages(host, port):
-    filename = 'messages.txt'
+async def read_chat_messages(host, port, filepath):
     try:
         reader, writer = await asyncio.open_connection(f'{host}', port)
 
@@ -20,7 +33,7 @@ async def read_chat_messages(host, port):
             message = f'{data.decode()!r}'
             date_now = f"[{datetime.strftime(datetime.now(), '%d.%m.%Y %H:%M')}]"
             print(date_now, message)
-            await write_to_file(filename, f'{date_now} {message}\n')
+            await write_to_file(filepath, f'{date_now} {message}\n')
 
         print('Close the connection')
         writer.close()
@@ -34,9 +47,17 @@ async def read_chat_messages(host, port):
 
 
 def main():
-    host = 'minechat.dvmn.org'
-    port = 5000
-    asyncio.run(read_chat_messages(host, port))
+    if os.path.isfile('.env'):
+        host = env.str('HOST')
+        port = env.int('PORT')
+        filepath = env.str('HISTORY', 'messages.txt')
+    else:
+        args = parse_arguments()
+        host = args.host
+        port = args.port
+        filepath = args.history
+
+    asyncio.run(read_chat_messages(host, port, filepath))
 
 
 if __name__ == '__main__':
