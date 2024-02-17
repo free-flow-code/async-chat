8e41242357f91d7701cbe12371cf6ebcdf3f7ed2
free-flow-code <wildmuskaria8@gmail.com>
2024-02-17 19:33:12 +0300

    Refactor code, split by reusable functions.

--- send_messages.py
+++ send_messages.py
@@ -6,48 +6,72 @@ import aiofiles
 from environs_processing import fetch_environs
 
 
-async def send_message(host, port, message, token):
+async def authorise(reader, writer, token):
+    writer.write(f'{token}\n'.encode())
+    await writer.drain()
+
+    response_message = await reader.readline()
+    assert json.loads('null') is None
+    authorise_data = json.loads(response_message.decode())
+
+    return authorise_data
+
+
+async def register(reader, writer):
+    username = input('Введите желаемое имя: ')
+    writer.write(f'{username}\n'.encode())
+
+    account_data = await reader.readline()
+    async with aiofiles.open('account_data.json', mode='w') as file:
+        await file.write(account_data.decode())
+
+    return json.loads(account_data.decode())
+
+
+async def submit_message(writer, message):
+    writer.write(f'{message}\n'.encode())
+    await writer.drain()
+
+
+async def connect_to_chat(host, port, message, token):
     try:
         reader, writer = await asyncio.open_connection(f'{host}', port)
         answer = await reader.readline()
         logging.debug(answer.decode())
 
-        if not os.path.isfile('account_data.json'):
-            writer.write("\n".encode())
-            await writer.drain()
-            await reader.readline()
-            username = input('Введите желаемое имя: ')
-            writer.write(f"{username}\n".encode())
-            account_data = await reader.readline()
-            logging.debug(account_data.decode())
-            async with aiofiles.open('account_data.json', mode="w") as file:
-                await file.write(account_data.decode())
-            return
+        if not token and os.path.isfile('account_data.json'):
+            async with aiofiles.open('account_data.json', mode='r') as file:
+                file_content = await file.read()
+                account_data = json.loads(file_content)
+                token = account_data['account_hash']
 
-        if token:
-            writer.write(f'{token}'.encode())
-            await writer.drain()
-            response_message = await reader.readline()
-            assert json.loads('null') is None
-            response_data = json.loads(response_message.decode())
-            if response_data is None:
-                logging.error('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-                return
+        if not await authorise(reader, writer, token):
+            logging.debug('Неизвестный токен. Регистрация нового пользователя.')
+            if await register(reader, writer):
+                logging.debug('Новый аккаунт зарегистрирован. Данные записаны в файл.')
 
-        writer.write(f"{message}\n\n".encode())
+        await submit_message(writer, message)
+        logging.debug('Сообщение отправлено.')
         writer.close()
         await writer.wait_closed()
 
+    except json.JSONDecodeError as err:
+        logging.error(f'Ошибка при декодировании JSON: {err}', exc_info=True)
     except ConnectionError as err:
-        logging.error(f'{err}')
+        logging.error(f'Обрыв соединения: {err}')
     except asyncio.CancelledError:
         logging.error('Cancelled.')
     except Exception as err:
-        logging.error(f'{err}')
+        logging.error(f'{err}', exc_info=True)
 
 
 def main():
-    logging.getLogger('asyncio').setLevel(logging.DEBUG)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='py_log.log',
+        filemode='w',
+        format='%(asctime)s %(levelname)s %(message)s'
+    )
     environs = fetch_environs()
     host = environs.get('host')
     port = environs.get('send_port')
@@ -57,7 +81,7 @@ def main():
     if not message:
         message = input('Введите сообщение: ')
 
-    asyncio.run(send_message(host, port, message, token))
+    asyncio.run(connect_to_chat(host, port, message, token))
 
 
 if __name__ == '__main__':
